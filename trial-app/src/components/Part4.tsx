50afa4f4fe376687396106a9719c1328836a6b9e
Saptarshi Paul <saptarshi2027paul@gmail.com>
2024-08-28 18:59:22 +0530

    3rd push
    c

---
+++ trial-app/src/components/Part4.tsx
@@ -0,0 +1,11 @@
+import {  Text } from 'react-native'
+import React from 'react'
+
+const Part4 = () => {
+  return (
+    
+      <Text>  I am currently persuaing Btech from   TMSL</Text>
+  )
+}
+
+export default Part4
